718bf484dca246c55df18f18b7a031dc0ba69a29
Zibeline <zibeline.dev@gmail.com>
2025-01-18 04:14:26 +0100

    📦 Recipes : Display only the best recipe for Tier 1

--- Scenes/Views/CraftingScene/recipes.gd
+++ Scenes/Views/CraftingScene/recipes.gd
@@ -3,8 +3,35 @@ extends VBoxContainer
 
 
 ##
+var _recipes : Dictionary[SwordRecipe.List, UICraftingRecipe] = {}
+
+
+##
 func _ready() -> void :
+	Signals.ref.recipes_updated.connect(_on_recipes_updated)
 	_instantiate_recipes()
+
+
+##
+func _update_recipes() -> void : 
+	_hide_all_recipes()
+	_show_available_recipes()
+
+
+##
+func _hide_all_recipes() -> void : 
+	for key : SwordRecipe.List in _recipes : 
+		_recipes[key].visible = false
+
+
+##
+func _show_available_recipes() -> void : 
+	var data : Data = Game.ref.data
+	
+	if data.sword_recipes_unlock[SwordRecipe.List.TIER_1_LEVEL_2] : 
+		_recipes[SwordRecipe.List.TIER_1_LEVEL_2].visible = true
+	elif data.sword_recipes_unlock[SwordRecipe.List.TIER_1_LEVEL_1] : 
+		_recipes[SwordRecipe.List.TIER_1_LEVEL_1].visible = true
 
 
 ##
@@ -14,4 +41,12 @@ func _instantiate_recipes() -> void  :
 	for recipe : SwordRecipe in SwordRecipe.recipes :
 		var node : UICraftingRecipe = scene.instantiate()
 		node.set_recipe(recipe)
+		_recipes[recipe.key] = node
 		add_child(node)
+	
+	_update_recipes()
+
+
+##
+func _on_recipes_updated() -> void : 
+	_update_recipes()
--- Scenes/Views/MainTabContainer/main_tab_container.gd
+++ Scenes/Views/MainTabContainer/main_tab_container.gd
@@ -7,3 +7,4 @@ func _input(event : InputEvent) -> void :
 	if event.is_action_pressed("navigate_to_mining") : current_tab = 0
 	if event.is_action_pressed("navigate_to_crafting") : current_tab = 1
 	if event.is_action_pressed("navigate_to_inventory") : current_tab = 2
+	if event.is_action_pressed("navigate_to_upgrades") : current_tab = 3
