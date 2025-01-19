a2074bac6784f2acc789702fadbc55fe196ef5c7
3434 <3434kayn@gmail.com>
2025-01-19 13:48:53 +0300

    // Added some Ores textures

--- Scripts/Abstracts/ore.gd
+++ Scripts/Abstracts/ore.gd
@@ -37,21 +37,21 @@ static func initialise_ores() -> Error :
 	var tier_1 : Ore = Ore.new()
 	tier_1.enum_value = List.TIER_1
 	tier_1.tier = 1
-	tier_1.name = "Copper"
-	tier_1.texture = load("res://Assets/Ores/ore.svg")
+	tier_1.name = "Iron Chunk"
+	tier_1.texture = load("res://Assets/Ores/IronChunk.png")
 	ores.append(tier_1)
 	
 	var tier_2 : Ore = Ore.new()
 	tier_2.enum_value = List.TIER_2
 	tier_2.tier = 2
-	tier_2.texture = load("res://Assets/Ores/minerals.png")
-	tier_2.name = "Iron"
+	tier_2.texture = load("res://Assets/Ores/CelestialSteel.png")
+	tier_2.name = "Celestial Steel"
 	ores.append(tier_2)
 	
 	var tier_3 : Ore = Ore.new()
 	tier_3.enum_value = List.TIER_3
 	tier_3.tier = 3
-	tier_3.name = "Silver"
+	tier_3.name = "Obsidian"
 	ores.append(tier_3)
 	
 	return OK
