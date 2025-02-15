0a64d9bded3eff128933209d404327742788f4e4
3434 <3434kayn@gmail.com>
2025-02-15 17:44:21 +0300

    // Some navigation button fixes

--- Scenes/Components/NavigationButton/navigation_button.gd
+++ Scenes/Components/NavigationButton/navigation_button.gd
@@ -1,4 +1,4 @@
-class_name NavigationButton extends VBoxContainer
+class_name NavigationButton extends Button
 
 
 signal navigation_requested(target : UserInterface.Views)
@@ -15,37 +15,39 @@ var colour_unfolded_modulate : Color = Color(1.0, 1.0, 1.0)
 
 
 func _ready() -> void :
-	(%TextureButton as TextureButton).pressed.connect(on_button_pressed)
+	(self as Button).pressed.connect(on_button_pressed)
 	
 	(%Label as Label).modulate.a = 0.0
-	(%TextureButton as TextureButton).modulate = colour_folded_modulate
-	(%TextureButton as TextureButton).texture_normal = texture
+	(%Texture as TextureRect).modulate = colour_folded_modulate
+	(%Texture as TextureRect).texture = texture
 	(%Label as Label).text = display_name
 
 
 
 func fold() -> void :
+	self.disabled = false
 	var texture_tween : Tween = create_tween()
-	texture_tween.tween_property(%TextureButton, "position:y", 0.0, 0.2).from(-8.0)
+	texture_tween.tween_property(%Texture, "position:y", 0.0, 0.15).from(-8.0)
 	texture_tween.set_parallel(true).\
-	tween_property(%TextureButton, "modulate", colour_folded_modulate, 0.2).\
+	tween_property(%Texture, "modulate", colour_folded_modulate, 0.2).\
 	from(colour_unfolded_modulate)
 	
 	var label_tween : Tween = create_tween()
-	label_tween.tween_property(%Label, "modulate:a", 0.0, 0.2).from(1.0)
+	label_tween.tween_property(%Label, "modulate:a", 0.0, 0.15).from(1.0)
 	
 	unfolded = false
 
 
 func unfold() -> void : 
+	self.disabled = true
 	var texture_tween : Tween = create_tween()
-	texture_tween.tween_property(%TextureButton, "position:y", -8.0, 0.2).from(0.0)
+	texture_tween.tween_property(%Texture, "position:y", -8.0, 0.15).from(0.0)
 	texture_tween.set_parallel(true).\
-	tween_property(%TextureButton, "modulate", colour_unfolded_modulate, 0.2).\
+	tween_property(%Texture, "modulate", colour_unfolded_modulate, 0.2).\
 	from(colour_folded_modulate)
 	
 	var label_tween : Tween = create_tween()
-	label_tween.tween_property(%Label, "modulate:a", 1.0, 0.2).from(0.0)
+	label_tween.tween_property(%Label, "modulate:a", 1.0, 0.15).from(0.0)
 	
 	unfolded = true
 
